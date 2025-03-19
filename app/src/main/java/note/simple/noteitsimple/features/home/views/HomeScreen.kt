d0c968aed16c7413de59bb5db72ad97bc8be4698
Flavius - Lucian Domide <>
2025-03-19 12:20:45 +0200

    Add fab padding

--- app/src/main/java/note/simple/noteitsimple/features/home/views/HomeScreen.kt
+++ app/src/main/java/note/simple/noteitsimple/features/home/views/HomeScreen.kt
@@ -1,7 +1,12 @@
 package note.simple.noteitsimple.features.home.views
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,9 +22,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import note.simple.noteitsimple.R
@@ -35,6 +42,13 @@ fun HomeScreen(
 ) {
     val noteListState = noteViewModel.noteListState.collectAsState()
     val noteValue = noteListState.value
+
+    val configuration = LocalConfiguration.current
+    val fabPaddingEnd = if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        48.dp // Moves FAB up in landscape mode
+    } else {
+        0.dp // Normal padding for portrait
+    }
 
     // Scaffold holds together different parts of the UI such as app bars and floating action buttons
     Scaffold(topBar = {
@@ -57,10 +71,16 @@ fun HomeScreen(
             }
         )
     }, floatingActionButton = {
-        FloatingActionButton(onClick = {
-            // Navigate to add note screen
-            navController.navigate(Routes.AddNote.name)
-        }) {
+        FloatingActionButton(
+            onClick = {
+                // Navigate to add note screen
+                navController.navigate(Routes.AddNote.name)
+            }, modifier = Modifier
+                .padding(
+                    end = WindowInsets.navigationBars.asPaddingValues()
+                        .calculateEndPadding(LayoutDirection.Rtl) + fabPaddingEnd
+                )
+        ) {
             Icon(Icons.Rounded.Add, contentDescription = stringResource(R.string.add_note_button))
         }
     }) { innerPadding ->
